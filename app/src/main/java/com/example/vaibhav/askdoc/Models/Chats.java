239009936a860f4a41ad90896a82855972508be2
vaibhav1seth <vaibhavk774@gmail.com>
2018-11-01 05:11:17 +0530

    added displaying of messages

---
+++ app/src/main/java/com/example/vaibhav/askdoc/Models/Chats.java
@@ -0,0 +1,42 @@
+package com.example.vaibhav.askdoc.Models;
+
+public class Chats {
+
+    private String sender;
+    private String reciever;
+    private String message;
+
+    public Chats(String sender, String reciever, String message) {
+        this.sender = sender;
+        this.reciever = reciever;
+        this.message = message;
+    }
+
+    public Chats() {
+
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReciever() {
+        return reciever;
+    }
+
+    public void setReciever(String reciever) {
+        this.reciever = reciever;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
